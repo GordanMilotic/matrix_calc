643e98e84820c1b6cfbe8e64107dad43c0e23172
Gordan Milotić <115399374+GordanMilotic@users.noreply.github.com>
2025-03-22 21:00:44 +0100

    addition, division

--- matrix_calc.c
+++ matrix_calc.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#define maxDimension 3
+#define maxDimension 3 //maksimalna dimnezija matrice cemo ben povecat
 
 int main(){
     int matrixDimension;
-    int matrixA[maxDimension][maxDimension], matrixB[maxDimension][maxDimension];
+    int matrixA[maxDimension][maxDimension], matrixB[maxDimension][maxDimension], sum[maxDimension][maxDimension], diff[maxDimension][maxDimension];
 
     printf("Unesite dimenziju matrice (1, 2 ili 3): ");
     scanf("%d", &matrixDimension);
@@ -33,7 +33,7 @@ int main(){
     printf("Unesite elemente druge matrice %dx%d:\n", matrixDimension, matrixDimension);
     for (int i = 0; i < matrixDimension; i++){
         for (int j = 0; j < matrixDimension; j++){
-            //printf("Element [%d][%d]: ", i + 1, j + 1);
+            //printf("Element [%d][%d]: ", i + 1, j + 1); prikaz elementa 
             scanf("%d", &matrixB[i][j]);
         }
     }
@@ -42,6 +42,36 @@ int main(){
     for(int i = 0; i < matrixDimension; i++){
         for(int j = 0; j < matrixDimension; j++){
             printf("%d ", matrixB[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(int i = 0; i < matrixDimension; i++){
+        for(int j = 0; j < matrixDimension; j++){
+            sum[i][j]=matrixA[i][j] + matrixB[i][j];
+        }
+    }
+
+    printf("Zbroj matrica:\n");
+    for(int i = 0; i < matrixDimension; i++){
+        for(int j = 0; j < matrixDimension; j++){
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    //if uvjet  da biramo operaciju ili eventualno switch case
+
+    for(int i = 0; i < matrixDimension; i++){
+        for(int j = 0; j < matrixDimension; j++){
+            diff[i][j]=matrixA[i][j] - matrixB[i][j];
+        }
+    }
+
+    printf("Razlika matrica:\n");
+    for(int i = 0; i < matrixDimension; i++){
+        for(int j = 0; j < matrixDimension; j++){
+            printf("%d ", diff[i][j]);
         }
         printf("\n");
     }
